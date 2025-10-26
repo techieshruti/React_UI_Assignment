17b03e15411c45d76af6af5a5c4d206a6f9a774a
techieshruti <shrutichandra721@gmail.com>
2025-10-26 22:06:31 +0530

    initial commit

---
+++ user-profile/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div className='container'>
+    <></>
+    <div className='profile'>
+      <div className='upper'></div>
+      <div className='lower'></div>
+    </div>
+   </div>
+  )
+}
+
+export default App
