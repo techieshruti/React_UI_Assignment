6b331e479da5110324e128a74a9a49fc4a745335
techieshruti <shrutichandra721@gmail.com>
2025-10-27 14:51:31 +0530

    added images to gallery tab

--- user-profile/src/components/GalleryWidget.jsx
+++ user-profile/src/components/GalleryWidget.jsx
@@ -2,10 +2,7 @@ import React, { useState } from "react";
 
 const GalleryWidget = () => {
   const [images, setImages] = useState([
-    "https://via.placeholder.com/150",
-    "https://via.placeholder.com/150",
-    "https://via.placeholder.com/150",
-  ]);
+   ]);
 
   const addImage = () => {
     const newImg = prompt("Enter image URL:");
@@ -13,9 +10,10 @@ const GalleryWidget = () => {
   };
 
   return (
+
     <div className="bg-[#3d4045] p-5 rounded-2xl shadow-lg max-w-[600px] w-full mt-6">
       <div className="flex items-center justify-between mb-4">
-        <h3 className="text-lg text-white font-semibold bg-gray-800 px-5 py-2 rounded-3xl">Gallery</h3>
+        <h3 className="text-lg text-white font-semibold bg-gray-800 px-5 py-2 rounded-xl">Gallery</h3>
         <button
           onClick={addImage}
           className="bg-gray-800 hover:bg-gray-700 px-3 py-1 rounded-lg text-sm text-gray-200 flex items-center gap-1"
@@ -28,10 +26,10 @@ const GalleryWidget = () => {
       <div className="grid grid-cols-3 gap-3">
         {images.map((src, idx) => (
           <div key={idx} className="overflow-hidden rounded-lg">
-            <img 
-              src={"https://image.lexica.art/full_webp/6d93facf-329c-44a1-8e96-40ac315227ce"}
+            <img
+              src={src}
               alt="Gallery"
-              className="w-full h-30 object-cover hover:scale-105 transition-transform duration-300 bg-slate-200"
+              className="w-full h-30 object-cover hover:scale-105 transition-transform duration-300"
             />
           </div>
         ))}
